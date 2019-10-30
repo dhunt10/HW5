c6836b1f58c383cc9205f2f165b2074f7bcbf9b4
Darin Hunt <dhunt10@gmail.com>
2019-10-29 23:35:29 -0400

    added changed from tuesday

--- src/edu/cs3500/spreadsheets/BeyondGood.java
+++ src/edu/cs3500/spreadsheets/BeyondGood.java
@@ -2,7 +2,15 @@ package edu.cs3500.spreadsheets;
 
 import edu.cs3500.spreadsheets.model.Cell;
 import edu.cs3500.spreadsheets.model.Coord;
+import edu.cs3500.spreadsheets.sexp.Parser;
+import edu.cs3500.spreadsheets.sexp.SList;
+import edu.cs3500.spreadsheets.sexp.SNumber;
+import edu.cs3500.spreadsheets.sexp.SSymbol;
+import edu.cs3500.spreadsheets.sexp.Sexp;
+import edu.cs3500.spreadsheets.sexp.SexpVisitor;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The main class for our program.
@@ -14,7 +22,7 @@ public class BeyondGood {
    */
   public static void main(String[] args) {
 
-    File infile = null;
+    /*File infile = null;
     Cell incell = null;
     for(int i = 0; i < args.length; i++){
       switch(args[i]){
@@ -38,9 +46,21 @@ public class BeyondGood {
     }
     if(incell == null || incell == null){
       throw new IllegalArgumentException("bro give us some inputs to work with");
-    }
+    }*/
 
     createSpreadSheet();
+    Sexp sexp = Parser.parse("(SUM (PROD 10 3) (SUM 10 8) )");
+    List<Sexp> sexps = new ArrayList<>();
+    sexps.add(new SSymbol("SUM"));
+    sexps.add(new SNumber(10));
+    sexps.add(new SNumber(1));
+    SList compare = new SList(sexps);
+    //tests = test.split(" ");
+    System.out.println(sexp.getClass().getSimpleName() == compare.getClass().getSimpleName());
+    System.out.println(sexp.toString().split(" ")[0]);
+
+
+
 
     /*
       TODO: For now, look in the args array to obtain a filename and a cell name,
